1d0c5161958cf3f2084bb081ae728e6f8020e5bb
junyeongyu <Junyeong.Yu@MyGeorgian.ca>
2017-09-06 22:00:00 -0400

    Add tutorial of builder pattern

---
+++ src/main/java/com/junyeong/yu/prototype/design_pattern/builder/MainBuilder.java
@@ -0,0 +1,14 @@
+package com.junyeong.yu.prototype.design_pattern.builder;
+
+public class MainBuilder {
+    public static void main(String[] args) {
+        Waiter waiter = new Waiter();
+        PizzaBuilder hawaiianPizzabuilder = new HawaiianPizzaBuilder();
+        PizzaBuilder spicyPizzaBuilder = new SpicyPizzaBuilder();
+
+        waiter.setPizzaBuilder( hawaiianPizzabuilder );
+        waiter.constructPizza();
+
+        Pizza pizza = waiter.getPizza();
+    }
+}
---
+++ src/main/java/com/junyeong/yu/prototype/design_pattern/builder/Waiter.java
@@ -0,0 +1,21 @@
+package com.junyeong.yu.prototype.design_pattern.builder;
+
+/* "Director" */
+class Waiter {
+    private PizzaBuilder pizzaBuilder;
+
+    public void setPizzaBuilder(PizzaBuilder pb) {
+        pizzaBuilder = pb;
+    }
+
+    public Pizza getPizza() {
+        return pizzaBuilder.getPizza();
+    }
+
+    public void constructPizza() {
+        pizzaBuilder.createNewPizzaProduct();
+        pizzaBuilder.buildDough();
+        pizzaBuilder.buildSauce();
+        pizzaBuilder.buildTopping();
+    }
+}
